1772913517831efe1ce062ef6b38a346572864df
someone <superwibr@gmail.com>
2020-06-19 14:05:27 -0400

    Updated Master

---
+++ autocommit.sh
@@ -0,0 +1,32 @@
+#====================================================================================
+# setup section
+
+# location must be set to the path from the user folder (e.g.- desktop/myproject)
+location="desktop/uncannyorange.github.io"
+
+# the ssh url of your github repository (you will have to have an ssh key set up on your account) (e.g.- git@github.com:myusername/myrepo.git)
+sshurl="git@github.com:uncannyorange/uncannyorange.github.io.git"
+#====================================================================================
+
+cd $location
+
+git init
+
+git add .
+
+echo 
+echo commiting to master ============================================================
+git commit -m "Updated Master"
+echo ================================================================================
+echo 
+
+echo pulling origin...
+git pull origin
+echo
+
+git remote set-url origin $sshurl
+echo pushing to master ==============================================================
+echo 
+git push -u origin master
+echo ================================================================================
+echo 
