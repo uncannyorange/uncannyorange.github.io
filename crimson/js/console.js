cf80065b057e9263b26efea74c96049dd0611c0c
uncannyorange <theuncannyorange@gmail.com>
2020-06-24 12:24:40 -0400

    Updated Master

--- crimson/js/console.js
+++ crimson/js/console.js
@@ -1,28 +1,22 @@
 var code = document.getElementById("crimsoncode").value;
 
-const cr001 = require('../revoltmain.html')
-const cr002 = require('../home.html')
-const cr003 = require('../qcdirectives.html')
-const cr004 = require('.../beta')
-const cr005 = require('../changelogu.html')
-
 function crcodetop(){
     var content = document.getElementById("$content");
     switch(code){
         case "CR-001":
-            content.setAttribute("src", cr001);
+            content.setAttribute("src", './revoltmain.html');
         break;
         case "CR-002":
-            content.setAttribute("src", cr002);
+            content.setAttribute("src", './home.html');
         break;
         case "CR-003":
-            content.setAttribute("src", cr003);
+            content.setAttribute("src", './qcdirectives.html');
         break;
         case "CR-004":
-            content.setAttribute("src", cr004);
+            content.setAttribute("src", '../beta');
         break;
         case "CR-005":
-            content.setAttribute("src", cr005);
+            content.setAttribute("src", './changelogu.html');
         break;
     }
 }
@@ -30,19 +24,19 @@ function crcodetop(){
 function crcode(){
     switch(code){
         case "CR-001":
-            window.location.href=cr001;
+            window.location.href='./revoltmain.html';
         break;
         case "CR-002":
-            window.location.href=cr002;
+            window.location.href='./home.html';
         break;
         case "CR-003":
-            window.location.href=cr003;
+            window.location.href='./qcdirectives.html';
         break;
         case "CR-004":
-            window.location.href=cr004;
+            window.location.href='../beta';
         break;
         case "CR-005":
-            window.location.href=cr005;
+            window.location.href='./changelogu.html';
         break;
     }
 }
